5a7afc78f46f782beb24192cf234202c4fa24409
aceDavon <makojidavid@gmail.com>
2022-09-12 23:18:15 +0100

    Install and create routing for application
    Create routes for the application using the react-router-dom from NPM

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import TodoContainer from './components/TodoContainer';
+import Landing from './Landing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />} path="/">
+          <Route element={<Landing />} index />
+          <Route element={<TodoContainer />} path="todos" />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );

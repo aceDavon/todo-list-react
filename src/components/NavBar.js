929e4ffa4fe822b8f292bb8a69da7deaba519cef
aceDavon <makojidavid@gmail.com>
2022-09-13 17:20:55 +0100

    Add active link to NavBar
    Modify the Navbar.js to allow for styling of the active link item

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Hamburger } from '../Icons';
 
 const NavBar = () => {
@@ -8,6 +8,25 @@ const NavBar = () => {
   const handleOpen = () => {
     setOpen(!open);
   };
+
+  const styledActive = 'text-red-500 bg-white p-4 rounded-lg';
+  const NavItems = [
+    {
+      id: 1,
+      label: 'Home',
+      path: '/',
+    },
+    {
+      id: 2,
+      label: 'Todos',
+      path: 'todos',
+    },
+    {
+      id: 3,
+      label: 'Contact',
+      path: 'contact',
+    },
+  ];
 
   return (
     <nav className="fixed top-0 left-0 right-0 px-12 lg:px-24 py-4 flex justify-between items-center shadow bg-transparent z-50">
@@ -26,15 +45,19 @@ const NavBar = () => {
             : 'hidden gap-4 items-center lg:flex'
         }
       >
-        <li className="transition-all ease-in delay-75 hover:-translate-y-1">
-          <Link to="/" className="text-white lg:text-blue-500 no-underline z-50" onClick={() => setOpen()}>Home</Link>
-        </li>
-        <li className="transition-all ease-in delay-75 hover:-translate-y-1">
-          <Link to="/todos" className="text-white lg:text-blue-500 no-underline z-50" onClick={() => setOpen()}>Todos</Link>
-        </li>
-        <li className="transition-all ease-in delay-75 hover:-translate-y-1">
-          <Link to="/contact" className="text-white lg:text-blue-500 no-underline z-50" onClick={() => setOpen()}>Contact</Link>
-        </li>
+        { NavItems.map((link) => (
+          <li key={link.id} className="transition-all ease-in delay-75 hover:-translate-y-1">
+            <NavLink
+              to={link.path}
+              className={({ isActive }) => (isActive
+                ? styledActive
+                : 'text-white lg:text-blue-500 no-underline z-50')}
+              onClick={() => setOpen()}
+            >
+              { link.label }
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   );
